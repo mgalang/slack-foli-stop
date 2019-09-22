ced031a8327a98a5d598fd8ca154b7f8eac43223
Marc Galang <marc.galang@wunder.io>
2019-09-22 21:45:22 +0300

    Get stopcode from text query parameter

--- busstop/main.go
+++ busstop/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -48,14 +49,24 @@ func getJSON(path string, result interface{}) error {
 	return json.NewDecoder(resp.Body).Decode(result)
 }
 
+func validateStopCode(code string) bool {
+	// Regex validation pattern.
+	r, _ := regexp.Compile("(^[0-9]+)|(T[0-9]+)|(PT[0-9]+)|(L[0-9]+)")
+
+	return r.MatchString(code)
+}
+
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
+func Handler(req events.APIGatewayProxyRequest) (Response, error) {
 	var buf bytes.Buffer
 
-	j := SiriJSON{}
+	var stopcode = req.QueryStringParameters["text"]
 
-	// TODO, use parameter.
-	var stopcode = "t4"
+	if validateStopCode(stopcode) {
+		return Response{StatusCode: 404}, errors.New("Invalid code")
+	}
+
+	j := SiriJSON{}
 
 	err := getJSON("https://data.foli.fi/siri/sm/"+stopcode, &j)
 
